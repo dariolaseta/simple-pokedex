f542cd674702603ad6b2331b06c1c9f3ba22056f
Dario La Seta <dario.laseta@gmail.com>
2023-10-26 01:26:39 +0200

    fixed hover effect

--- src/components/PokedexCard.vue
+++ src/components/PokedexCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card text-center my-4 p-2">
+    <div class="card text-center my-4 p-2 my_hover_effect">
         <h1 class="text-capitalize">{{ pokemonInfo.name }}</h1>
 
         <div class="fs-4 text-capitalize" v-for="(types, index) in pokemonData.types">
@@ -51,5 +51,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    
+div.my_hover_effect{
+    transition: all .3s ease-in-out;
+    z-index: 0;
+}
+
+div.my_hover_effect:hover{
+    cursor: pointer;
+    transform: scale(1.3);
+    z-index: 1;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+}
 </style>
