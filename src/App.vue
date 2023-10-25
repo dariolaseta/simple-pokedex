7d576e5c5a8c34106267ae20744d4831b2702e9f
Dario La Seta <dario.laseta@gmail.com>
2023-10-25 23:46:54 +0200

    added axios

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,26 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  
+  data() {
+    return {
+      apiUrl: "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0",
+      pokedex: []
+    }
+  },
+  methods: {
+    getPokedexApi(){
+      axios.get(this.apiUrl)
+      .then((response)=> {
+        console.log(response.data);
+      })
+    }
+  },
+  mounted() {
+    this.getPokedexApi();
+  },
 }
 </script>
 
