690b416b4b01e8132fa9f1ca2cdc17ab3494613d
Dario La Seta <dario.laseta@gmail.com>
2023-10-26 01:25:02 +0200

    started hover effect

--- src/components/Pokemons.vue
+++ src/components/Pokemons.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container justify-content-center">
         <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-12" v-for="(pokemon, index) in pokedex" :key="pokemon.name">
+            <div class="col-lg-4 col-md-6 col-sm-12 my_hover_effect" v-for="(pokemon, index) in pokedex" :key="pokemon.name">
                 <PokedexCard :pokemonInfo = "{
                     name: pokemon.name,
                     url: `https://pokeapi.co/api/v2/pokemon/${index + 1}/`
@@ -52,5 +52,15 @@ created() {
 </script>
 
 <style lang="scss" scoped>
-    
+    div.my_hover_effect{
+        transition: all .3s ease-in-out;
+        z-index: 0;
+    }
+
+    div.my_hover_effect:hover{
+        cursor: pointer;
+        transform: scale(1.3);
+        z-index: 1;
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    }
 </style>
