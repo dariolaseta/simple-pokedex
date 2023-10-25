f1ea0214c11ce2169048e633e97d7c7591cfe7e2
Dario La Seta <dario.laseta@gmail.com>
2023-10-26 01:27:48 +0200

    created footer component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <HeaderApp />
   <Pokemons />
+  <FooterApp />
 </template>
 
 <script>
 import HeaderApp from './components/HeaderApp.vue';
 import Pokemons from './components/Pokemons.vue';
+import FooterApp from './components/FooterApp.vue';
 
 export default {
   name: 'App',
 
   components:{
     HeaderApp,
-    Pokemons
+    Pokemons,
+    FooterApp
   },
 }
 </script>
