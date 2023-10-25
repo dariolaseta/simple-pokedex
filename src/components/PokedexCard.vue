d8ffce5a7b0b7e654efa7e098d6cfc19429a1297
Dario La Seta <dario.laseta@gmail.com>
2023-10-26 01:20:59 +0200

    added types

--- src/components/PokedexCard.vue
+++ src/components/PokedexCard.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="card text-center my-4 p-2">
         <h1 class="text-capitalize">{{ pokemonInfo.name }}</h1>
+
+        <div class="fs-4 text-capitalize" v-for="(types, index) in pokemonData.types">
+            <span>Type {{ index + 1 }}: {{ types.type.name }}</span>
+        </div>
+
         <span class="fs-4 text-capitalize">Pokedex number: {{ pokemonData.id }}</span>
+
         <div class="fs-4 text-capitalize" v-for="(abilities, index) in pokemonData.abilities">
             <span v-if="!abilities.is_hidden">Ability {{ index + 1 }}: {{ abilities.ability.name }}</span>
             <span v-else>Hidden Ability: {{ abilities.ability.name }}</span>
